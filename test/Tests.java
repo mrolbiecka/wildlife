f754bfbd89e8dc54b7e9083ecfe479363fadee26
Marta <martarolbiecka02@gmail.com>
2025-05-01 22:30:58 +0200

    Included tests

---
+++ test/Tests.java
@@ -0,0 +1,21 @@
+import com.wildlife.Board;
+import com.wildlife.organisms.animals.herbivores.Boar;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class Tests {
+
+    @Test
+    public void testAnimalMove() {
+        int startingPosX = 1;
+        int startingPosY = 1;
+        Board board = new Board(10, 10);
+        Boar b = new Boar(startingPosX, startingPosY, board);
+
+        b.performAction();
+
+        assertTrue(b.getPositionX() != startingPosX || b.getPositionY() != startingPosY);
+    }
+}
+//testy najpierw dane, potem metoda (jedna), assert (sprawdzenie)
